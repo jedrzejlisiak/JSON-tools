e1725db8940b5224af6ff4785eee97b22b44d468
Ja <wafellini@protonmail.com>
2020-12-15 23:56:23 +0100

    Porównywanie 2 stringów linia po linii
    Zwraca każdą linię (i numer), w której pliki się różnią jako stringa

--- src/main/java/pl/put/poznan/tools/logic/JSONComparator.java
+++ src/main/java/pl/put/poznan/tools/logic/JSONComparator.java
@@ -11,24 +11,64 @@ public class JSONComparator {
     public String checkFiles(){
 
         if (f1.equals("") && f2.equals("")){
-            return("oba jsony puste");
+            return("Oba jsony są puste");
         }
         else if (f1.equals("") || f2.equals("")){
-            return("jeden z jsonów jest pusty");
+            return("Jeden z jsonów jest pusty");
         }
         else if (f1.equals(f2)){
-            return("jsony są identyczne");
+            return("Jsony są identyczne");
         }
         else{
             compareStr(f1,f2);
-            return("porównywanie brrrrr");
+            return("Porównywanie brrrrr");
 
         }
 
     }
 
-    public void compareStr(String f1, String f2){
+    public String comparation(String f1, String f2, int n) {
+
+        if (f1.equals(f2)){
+            return "";
+        }
+        else{
+            return String.format("Unequal line nr \"%d\":    ",n)+ f1 + "    " + f2 + "\n";
+        }
 
     }
+
+    public String compareStr(String f1, String f2){
+        String lines1[] = f1.split("\\r?\\n");
+        String lines2[] = f2.split("\\r?\\n");
+
+        int l1 = lines1.length;
+        int l2 = lines2.length;
+
+        String output = "";
+
+        if (l1 > l2) {
+            for (int i = 0; i < l2; i++) {
+                output +=  comparation(lines1[i] ,lines2[i], i);
+
+            }
+            for (int j = l2; j < l1; j++) {
+                output +=  comparation(lines1[j], " ", j);
+            }
+
+        }
+        else{
+            for (int i = 0; i < l1; i++) {
+                output +=  comparation(lines1[i] ,lines2[i], i);
+            }
+            for (int j = l1; j < l2; j++) {
+                output +=  comparation(" ", lines2[j], j);
+            }
+        }
+
+        return output;
+
+    }
+
 
 }
