b6820840a6e4e89395386e92c0789a041eeebe6d
Ja <wafellini@protonmail.com>
2020-12-17 23:18:03 +0100

    Add and remove
    -added documentation needed for javadoc
    -removed a method which essentially did nothing

--- src/main/java/pl/put/poznan/tools/logic/JSONComparator.java
+++ src/main/java/pl/put/poznan/tools/logic/JSONComparator.java
@@ -1,28 +1,38 @@
 package pl.put.poznan.tools.logic;
 
+/**
+ *  Class purpose is to find differences in two JSON files
+ */
+
+
 public class JSONComparator {
 
     private String f1,f2;
+
+    /**
+     *  This is a constructor
+     *
+     * @param ff1 f1 is a JSON file fed to the function as a string
+     * @param ff2 f2 is a JSON file fed to the function as a string
+     */
+
     public JSONComparator(String ff1, String ff2){
         f1 = ff1;
         f2 = ff2;
     }
 
-    public String checkFiles(){
 
-        if (f1.equals("") && f2.equals("")){
-            return "Oba jsony są puste";
-        }
-        else if (f1.equals(f2)){
-            return "Jsony są identyczne";
-        }
-        else{
-            return compareStr();
-
-
-        }
-
-    }
+    /**
+     * This method compares two lines from different JSON files
+     *
+     * @param f1 f1 is a line from the first file fed to the function as a string
+     * @param f2 f2 is a line from the second file fed to the function as a string
+     * @param n n is the index of lines which is currently being compared
+     *
+     *
+     * @return returns either an empty string in case the lines do not differ, or
+     * or a string which includes the index of the line and both lines
+     */
 
     public String comparation(String f1, String f2, int n) {
         int l1 = f1.length();
@@ -47,7 +57,13 @@ public class JSONComparator {
 
     }
 
-    public String compareStr(){
+    /**
+     *  This method finds differences in two JSON files
+     *
+     *  @return returns lines which differ in both files and their index
+     */
+
+    public String checkFiles(){
         String s1 = new JSONDeminification(new JSONComponentImp(f1)).decorate();
         String s2 = new JSONDeminification(new JSONComponentImp(f2)).decorate();
         f1 = s1;
